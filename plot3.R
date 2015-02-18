cb55cad13edafad15af8d17212ded08aefa0c738
Mason Silber <mason@twitter.com>
2015-02-18 16:24:09 -0500

    Added script to make plot3

---
+++ plot3.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimoreData <- NEI[NEI$fips == "24510", ]
+baltimoreData$type <- as.factor(baltimoreData$type)
+colnames(baltimoreData)[[5]] <- "Type" # So the name of the legend is capitalized
+
+aggregatedData <- aggregate(Emissions ~ year + Type, baltimoreData, sum)
+plot <- ggplot(aggregatedData, aes(year, Emissions, color=Type))
+plot <- plot + geom_line() + xlab("Year") + ylab("Emissions (tons)") + ggtitle("PM2.5 Emissions - Baltimore, MD")
+print(plot)
