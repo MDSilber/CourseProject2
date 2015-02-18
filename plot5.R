02f848b20d47d64173aeebf0ffdb0f29a78d6df3
Mason Silber <mason@twitter.com>
2015-02-18 18:04:49 -0500

    Added script to make plot5

---
+++ plot5.R
@@ -0,0 +1,9 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimoreData <- NEI[NEI$fips == "24510", ]
+motorCodes <- SCC[grepl("On-Road", SCC$EI.Sector), ]$SCC
+emissionSubset <- baltimoreData[baltimoreData$SCC %in% motorCodes, ]
+emissionSums <- sapply(unique(emissionSubset$year), function(x) { sum(emissionSubset[emissionSubset$year == x, ]$Emission) } )
+data <- data.frame("Year" = unique(NEI$year), "Emissions" = emissionSums)
+barplot(data$Emissions, names.arg = data$Year, xlab="Year", ylab="Emissions (tons)", ylim=c(0,350), main="Motor-related PM2.5 Emissions\nBaltimore, MD")
